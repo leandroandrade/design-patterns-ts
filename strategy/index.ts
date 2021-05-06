61945f57a43879615a463b4f4ba183502034ace9
leandroandrade <leandro.andrade2588@gmail.com>
2021-05-06 20:33:59 -0300

    comments

--- strategy/index.ts
+++ strategy/index.ts
@@ -3,12 +3,13 @@ import { ChristmasStrategy } from './strategies/christmas-strategy';
 import { BlackfridayStrategy } from './strategies/blackfriday-strategy';
 import { DefaultStrategy } from './strategies/default-strategy';
 
-// change the attribute active to true and see the behavior
+// estrategias de vendas definidas pelo RH para descontos.
 const promos = [
     { promo: 'christmas', active: false },
     { promo: 'blackfriday', active: false },
 ];
 
+// caso exista a necessidade de mais uma estrategia, basta adicionar o comporamento no array abaixo.
 const strategies = {
     christmas: new ChristmasStrategy(),
     blackfriday: new BlackfridayStrategy(),
@@ -17,6 +18,8 @@ const strategies = {
 
 async function run(client: any) {
     const active = promos.find((campain) => campain.active);
+
+    // caso todas as estrategias estejam desativadas, utilizamos a estrategia default
     const strategy = strategies[active?.promo] || strategies.default;
 
     const contextStrategy = new ContextStrategy(strategy);
